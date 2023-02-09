f3b57933bada26e4c6b7b3663c851f516fa56857
codewitgabi <gabrielmicheal497@gmail.com>
2023-02-09 11:26:43 +0100

    Updated password change form

--- account/forms.py
+++ account/forms.py
@@ -50,32 +50,12 @@ class CustomAdminPasswordChangeForm(
 	AdminPasswordChangeForm):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		print(dir(self.fields["password2"]))
 		self.fields["password1"].help_text = None
 		self.fields["password2"].help_text = None
-		self.fields["password2"].errors = None
 		
 	class Meta:
 		model = User
 		fields = "__all__"
-		
-	def clean_password1(self):
-		symbols = "@#_~[]{}()$&?%/"
-		password = self.cleaned_data.get("password1")
-		
-		# MinimumLengthValidator
-		if len(password) < 10:
-			raise forms.ValidationError("Password is too short.")
-		
-		# CommonPasswordValidator
-		if password.isdigit() or password.isalpha():
-			raise forms.ValidationError("Password is too common.")
-		
-		# NoSymbolValidator
-		if not any([sym in symbols for sym in password]):
-			raise forms.ValidationError(f"Password should contain any of {symbols}")
-			
-		return password;
 		
 
 class CustomPasswordChangeForm(PasswordChangeForm):
@@ -84,39 +64,9 @@ class CustomPasswordChangeForm(PasswordChangeForm):
 		print(self.fields)
 		self.fields["old_password"].help_text = None
 		self.fields["new_password1"].help_text = None
-		self.fields["new_password2"].help_text = None
-		
-	new_password1 = forms.CharField(
-		label="",
-		required=False,
-		widget=forms.PasswordInput(attrs={
-			"style": "display: none;"
-		})
-	)
 		
 	class Meta:
 		model = User
-		fields = ["old_password", "new_password1"]
+		fields = "__all__"
 		
-	def clean_new_password1(self):
-		symbols = "@#_~[]{}()$&?%/"
-		password = self.cleaned_data.get("new_password1")
-		
-		""" check if old password equals new password """
-		if self.cleaned_data.get("old_password") == password:
-			raise forms.ValidationError("New password cannot have same value as old password!")
-		
-		""" MinimumLengthValidator """
-		if len(password) < 10:
-			raise forms.ValidationError("Password is too short.")
-		
-		""" CommonPasswordValidator """
-		if password.isdigit() or password.isalpha():
-			raise forms.ValidationError("Password is too common.")
-		
-		""" NoSymbolValidator """
-		if not any([sym in symbols for sym in password]):
-			raise forms.ValidationError(f"Password should contain any of {symbols}")
-			
-		return password;
 		
